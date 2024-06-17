b593a9a82cdfe9ae6e50044a76b01df47b9f9036
VoDev01 <vodev1405@gmail.com>
2024-06-17 22:00:12 +0300

    Views of admin panel

---
+++ resources/views/admin/products.blade.php
@@ -0,0 +1,27 @@
+<x-admin-layout>
+    <x-slot name="title">
+        Админ панель - Товары | Silerium
+    </x-slot>
+    <div class="container">
+        <div class="row">
+            <a class="btn col" data-bs-toggle="collapse" href="#products" aria-expanded="false">Товары</a>
+
+            <nav>
+                
+            </nav>
+
+            <a class="btn col" data-bs-toggle="collapse" data-bs-target="#categories" href="#categories" aria-expanded="false">Категории</a>
+
+            <nav>
+                
+            </nav>
+
+            <a class="btn col" data-bs-toggle="collapse" data-bs-target="#subcategorits" href="#subcategories" aria-expanded="false">Подкатегории</a>
+
+            <nav>
+                
+            </nav>
+
+        </div>
+    </div>
+</x-admin-layout>
