baa178e72f29a32567478eb198cc5ebb673c91ab
vodev <vodev1405@gmail.com>
2024-12-29 15:09:06 +0300

    Setted up project on linux.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,7 +39,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\CheckBannedUser::class,
-            \App\Http\Middleware\TimezoneBasedOnIP::class
+       //     \App\Http\Middleware\TimezoneBasedOnIP::class
         ],
 
         'api' => [
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,7 @@
 import './bootstrap';
-import $ from '$';
 import jQuery from 'jquery';
 import toastr from 'toastr';
 import StarRating from 'star-rating.js';
-window.$ = jQuery;
+window.$ = window.jQuery = jQuery;
 window.toastr = toastr;
 window.StarRating = StarRating;
