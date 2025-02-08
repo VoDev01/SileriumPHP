14038613d9015e4e6a3675b52c3a6795ae358653
Vlad Osipenko <vodev1405@gmail.com>
2025-02-08 23:56:35 +0300

    Cart clear after payment

--- routes/web/payment.php
+++ routes/web/payment.php
@@ -7,7 +7,7 @@ Route::controller(PaymentController::class)->group(function(){
     Route::get('credentials', 'credentialsForm');
     Route::get('send_confirmation_token', 'sendConfirmationToken')->name('payment.sendConfirmationToken');
     Route::get('receive_order_id', 'receiveOrderId')->name('payment.receiveOrderId');
-    Route::post('receive_confirmation_token', 'receiveConfirmationToken');
+    Route::post('receive_confirmation_token', 'receiveConfirmationToken')->name('payment.receiveConfirmationToken');
     Route::post('create_payment_request', 'createPaymentRequest')->name('payment.createPaymentRequest');
     Route::get('finished', 'finished');
 });
